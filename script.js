47f878cf4c13ee88b72e549f3793b6080dd3288c
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-11 18:40:40 +0300

    - created elements in javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+var doboz = document.getElementById("doboz");
+var projects = document.getElementsByClassName("projects");
+var twoButton = document.getElementsByClassName("twoButton");
+
+var myProjects = document.getElementsByClassName("myProjects");
+var contact = document.getElementsByClassName("contact");
+
+doboz.appendChild(projects);
+doboz.appendChild(twoButton);
+
